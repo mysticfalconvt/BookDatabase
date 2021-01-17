eec9814b03283ba24e9462d56ae0191eabd30dab
MysticfalconVT <rboskind@gmail.com>
2021-01-17 15:32:05 -0500

    metadata update

--- book-database-gatsby/gatsby-config.js
+++ book-database-gatsby/gatsby-config.js
@@ -2,6 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Best Book Database Ever",
     description: "Social Justice Book Search",
+    siteUrl: "bookdatabase.netlify.app",
   },
   plugins: [
     {
